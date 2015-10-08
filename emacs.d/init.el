7cb12515336c47e2b2475f486abdd880ed914ecd
hefesto <daherrera@bluemessaging.com>
2015-10-08 13:21:57 -0500

    Tronó XMonad porque borré la carpeta de ghc, y ahorita estoy preparando para hacer el backup de todo.

---
+++ emacs.d/init.el
@@ -0,0 +1,361 @@
+
+(let ((default-directory "~/.emacs.d/Git-pluggins/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(setenv "PATH" (concat "/usr/local/bin:/opt/local/bin:/usr/bin:/bin" (getenv "PATH")))
+(require 'cl)
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list 'package-archives
+	       '("tromey" . "http://tromey.com/elpa/") t)
+  (package-initialize))
+
+(setq package-archive-enable-alist '(("melpa" deft magit)))
+
+;; Download the ELPA archive description if needed.
+;; This informs Emacs about the latest versions of all packages, and
+;; makes them available for download.
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; The packages you want installed. You can also install these
+;; manually with M-x package-install
+;; Add in your own as you wish:
+(defvar my-packages
+  '(;; makes handling lisp expressions much, much easier
+    ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
+    paredit
+
+    ;; key bindings and code colorization for Clojure
+    ;; https://github.com/clojure-emacs/clojure-mode
+    clojure-mode
+
+    ;; extra syntax highlighting for clojure
+    clojure-mode-extra-font-locking
+
+    ;; integration with a Clojure REPL
+    ;; https://github.com/clojure-emacs/cider
+    cider
+
+    ;; allow ido usage in as many contexts as possible. see
+    ;; customizations/navigation.el line 23 for a description
+    ;; of ido
+    ido-ubiquitous
+
+    ;; Enhances M-x to allow easier execution of commands. Provides
+    ;; a filterable list of possible commands in the minibuffer
+    ;; http://www.emacswiki.org/emacs/Smex
+    smex
+
+    ;; project navigation
+    projectile
+
+    ;; colorful parenthesis matching
+    rainbow-delimiters
+
+    ;; edit html tags like sexps
+    tagedit
+
+    ;; git integration
+    magit))
+
+;; On OS X, an Emacs instance started from the graphical user
+;; interface will have a different environment than a shell in a
+;; terminal window, because OS X does not run a shell during the
+;; login. Obviously this will lead to unexpected results when
+;; calling external utilities like make from Emacs.
+;; This library works around this problem by copying important
+;; environment variables from the user's shell.
+;; https://github.com/purcell/exec-path-from-shell
+(if (eq system-type 'darwin)
+    (add-to-list 'my-packages 'exec-path-from-shell))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(setq inhibit-startup-message t)
+
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+
+(global-linum-mode 1)
+
+(load-theme 'zenburn t)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+(setq user-full-name "Daniel Herrera Rendón")
+(setq user-mail-address "daniel.herrera.rendon@gmail.com")
+
+(setq tab-width 2
+      indent-tabs-mode nil)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(show-paren-mode t)
+
+(ido-mode t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers t)
+(require 'autopair)
+
+(require 'auto-complete-config)
+(ac-config-default)
+
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (indent-buffer)
+  (untabify-buffer)
+  (delete-trailing-whitespace))
+
+(defun cleanup-region (beg end)
+  "Remove tmux artifacts from region."
+  (interactive "r")
+  (dolist (re '("\\\\│\·*\n" "\W*│\·*"))
+    (replace-regexp re "" nil beg end)))
+
+(global-set-key (kbd "C-x M-t") 'cleanup-region)
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+
+;; make whitespace-mode use just basic coloring
+(setq whitespace-style (quote (spaces tabs newline space-mark tab-mark newline-mark)))
+
+(setq whitespace-display-mappings
+      ;; all numbers are Unicode codepoint in decimal. try (insert-char 182 ) to see it
+      '(
+        (space-mark 32 [183] [46]) ; 32 SPACE, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+        (newline-mark 10 [182 10]) ; 10 LINE FEED
+        (tab-mark 9 [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
+        ))
+(global-whitespace-mode 1)
+
+(setq flyspell-issue-welcome-flag nil)
+(if (eq system-type 'darwin)
+    (setq-default ispell-program-name "/usr/local/bin/aspell")
+  (setq-default ispell-program-name "/usr/bin/aspell"))
+(setq-default ispell-list-command "list")
+
+(add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
+
+(add-to-list 'auto-mode-alist '("\\.gitconfig$" . conf-mode))
+
+(add-to-list 'auto-mode-alist '("\\.hbs$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
+
+(defun my-export-to-parent ()
+  "Exports the table in the current buffer back to its parent DSV file and
+    then closes this buffer."
+  (let ((buf (current-buffer)))
+    (org-table-export parent-file export-func)
+    (set-buffer-modified-p nil)
+    (switch-to-buffer (find-file parent-file))
+    (kill-buffer buf)))
+
+(defun my-edit-dsv-as-orgtbl (&optional arg)
+  "Convet the current DSV buffer into an org table in a separate file. Saving
+    the table will convert it back to DSV and jump back to the original file"
+  (interactive "P")
+  (let* ((buf (current-buffer))
+         (file (buffer-file-name buf))
+         (txt (substring-no-properties (buffer-string)))
+         (org-buf (find-file-noselect (concat (buffer-name) ".org"))))
+    (save-buffer)
+    (with-current-buffer org-buf
+      (erase-buffer)
+      (insert txt)
+      (org-table-convert-region 1 (buffer-end 1) arg)
+      (setq-local parent-file file)
+      (cond 
+       ((equal arg '(4)) (setq-local export-func "orgtbl-to-csv"))
+       ((equal arg '(16)) (setq-local export-func "orgtbl-to-tsv"))
+       (t (setq-local export-func "orgtbl-to-tsv")))
+      (add-hook 'after-save-hook 'my-export-to-parent nil t))
+    (switch-to-buffer org-buf)
+    (kill-buffer buf)))
+
+;; Open the current TSV file as an Org table
+(global-set-key (kbd "C-c |") 'my-edit-dsv-as-orgtbl)
+
+
+;zsh
+
+(setq multi-term-program "/bin/zsh")
+
+(add-hook 'term-mode-hook
+          (lambda ()
+            (setq term-buffer-maximum-size 10000)))
+(add-hook 'term-mode-hook
+          (lambda ()
+            (setq show-trailing-whitespace nil)
+            (autopair-mode -1)))
+
+(defcustom term-unbind-key-list
+  '("C-z" "C-x" "C-c" "C-h" "C-y" "<ESC>")
+  "The key list that will need to be unbind."
+  :type 'list
+  :group 'multi-term)
+
+(defcustom term-bind-key-alist
+  '(
+    ("C-c C-c" . term-interrupt-subjob)
+    ("C-p" . previous-line)
+    ("C-n" . next-line)
+    ("C-s" . isearch-forward)
+    ("C-r" . isearch-backward)
+    ("C-m" . term-send-raw)
+    ("M-f" . term-send-forward-word)
+    ("M-b" . term-send-backward-word)
+    ("M-o" . term-send-backspace)
+    ("M-p" . term-send-up)
+    ("M-n" . term-send-down)
+    ("M-M" . term-send-forward-kill-word)
+    ("M-N" . term-send-backward-kill-word)
+    ("M-r" . term-send-reverse-search-history)
+    ("M-," . term-send-input)
+    ("M-." . comint-dynamic-complete))
+  "The key alist that will need to be bind.
+If you do not like default setup, modify it, with (KEY . COMMAND) format."
+  :type 'alist
+  :group 'multi-term)
+
+(add-hook 'term-mode-hook
+          (lambda ()
+            (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
+            (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))
+	    (define-key term-raw-map (kbd "C-y") 'term-paste)
+	    (yas-minor-mode 0)
+	    (message "%s" "This is in term mode and hook enabled.")))
+
+;; eclim
+(require 'eclim)
+(global-eclim-mode)
+(require 'eclimd)
+
+(setq help-at-pt-display-when-idle t)
+(setq help-at-pt-timer-delay 0.1)
+(help-at-pt-set-timer)
+
+;; add the emacs-eclim source
+(require 'ac-emacs-eclim-source)
+(ac-emacs-eclim-config)
+
+(defvar my-fullscreen-p t "Check if fullscreen is on or off")
+
+(defun my-non-fullscreen ()
+  (interactive)
+  (if (fboundp 'w32-send-sys-command)
+	  ;; WM_SYSCOMMAND restore #xf120
+	  (w32-send-sys-command 61728)
+	(progn (set-frame-parameter nil 'width 82)
+		   (set-frame-parameter nil 'fullscreen 'fullheight))))
+
+(defun my-fullscreen ()
+  (interactive)
+  (if (fboundp 'w32-send-sys-command)
+	  ;; WM_SYSCOMMAND maximaze #xf030
+	  (w32-send-sys-command 61488)
+	(set-frame-parameter nil 'fullscreen 'fullboth)))
+
+(defun my-toggle-fullscreen ()
+  (interactive)
+  (setq my-fullscreen-p (not my-fullscreen-p))
+  (if my-fullscreen-p
+	  (my-non-fullscreen)
+    (my-fullscreen)))
+
+(my-fullscreen)
+
+;; backups not on .
+(setq backup-directory-alist `(("." . "~/temp")))
+(setq auto-save-file-name-transforms
+      `((".*" ,"~/temp/" t)))
+
+;;irc
+(require 'erc)
+
+;; check channels
+(erc-track-mode t)
+(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
+
+                                 "324" "329" "332" "333" "353" "477"))
+;; don't show any of this
+(setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
+
+;; antlr grammar to antlr-mode
+(add-to-list 'auto-mode-alist '("\\.g4\\'" . antlr-mode))
+
+;; clojure files to CIDER mode.
+(add-to-list 'auto-mode-alist '("\\.clj\\'" . clojure-mode))
+
+(add-to-list 'load-path "/home/hefesto/.emacs.d/elpa/clojure-mode-extra-font-locking-20150110.505/")
+(load "/home/hefesto/.emacs.d/elpa/clojure-mode-extra-font-locking-20150110.505/clojure-mode-extra-font-locking")
+(require 'clojure-mode)
+;;(require 'rainbow-delimiters-mode)
+(defun turn-on-clojure-minor-modes ()
+  (paredit-mode 1)
+  (rainbow-delimiters-mode 1)
+  (cider-mode 1))
+  ;;(clojure-mode-extra-font-locking 1))
+(add-hook 'clojure-mode-hook 'turn-on-clojure-minor-modes)
+
+(add-hook 'cider-repl-mode-hook #'paredit-mode)
+(add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
+
+
+(require 'cc-mode)
+;;(setq c-default-style "linux"
+;;      c-basic-offset 4)
+(setq-default c-indent-tabs-mode t     ; Pressing TAB should cause indentation
+	      c-indent-level 4         ; A TAB is equivilent to four spaces
+	      c-argdecl-indent 0       ; Do not indent argument decl's extra
+	      c-tab-always-indent t
+	      backward-delete-function nil) ; DO NOT expand tabs when deleting
+;;(c-add-style "my-c-style" '((c-continued-statement-offset 4))) ; If a statement continues on the next line, indent the continuation by 4
+(defun my-c-mode-hook ()
+  (c-set-style "linux")
+  (setq c-basic-offset 4)
+  (c-set-offset 'substatement-open '0) ; brackets should be at same indentation level as the statements they open
+  (c-set-offset 'inline-open '+)
+  (c-set-offset 'block-open '+)
+  (c-set-offset 'brace-list-open '+)   ; all "opens" should be indented by the c-indent-level
+  (c-set-offset 'case-label '+))       ; indent case labels by c-indent-level, too
+(add-hook 'c-mode-hook 'my-c-mode-hook)
+(add-hook 'c++-mode-hook 'my-c-mode-hook)
+
+(require 'yasnippet)
+(add-to-list 'yas-snippet-dirs "~/.emacs.d/yasnippet-snippets")
+
+(yas-global-mode 1)
+
+(if (not (get-buffer "*terminal<1>*"))
+    (multi-term))
+
+
+
+
+
---
+++ xmonad/xmonad.hs
@@ -0,0 +1,27 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeysP)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        } `additionalKeysP`
+        [ ("C-<Print>", spawn "sleep 0.1; scrot -s")
+        , ("<Print>", spawn "scrot")
+--        , ("M-j", spawn "xdotool key Down")
+--        , ("M-k", spawn "xdotool key Up")
+--        , ("M-l", spawn "xdotool key Right")
+--        , ("M-h", spawn "xdotool key Left")
+        ]
+--        , ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+
