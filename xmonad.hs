682b86b18a9043d936ae57e56158763769759290
hhefesto <daniel.herrera.rendon@gmail.com>
2017-12-15 17:20:41 -0600

    Terminal gnome y xmobar ya no se cubre por los programas abiertos.

--- xmonad.hs
+++ xmonad.hs
@@ -8,18 +8,20 @@ import System.IO
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
+        { modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        , ppTitle = xmobarColor "darkgreen" "" . shorten 20
                         }
-        , modMask = mod4Mask     -- Rebind Mod to the Windows key
         , normalBorderColor  = "#4a4a4a"
         , focusedBorderColor = "#7fff00"
+        , terminal = "gnome-terminal"
         } `additionalKeysP`
-        [ ("C-<Print>", spawn "sleep 0.1; scrot -s") --Esto no funciona. Tampoco sé para qué es.
-        , ("<Print>", spawn "scrot")
+        [ ("<Print>", spawn "scrot")
+--        , ("C-<Print>", spawn "sleep 0.1; scrot -s") --Esto no funciona. Tampoco sé para qué es.
 --        , ("M-j", spawn "xdotool key Down")
 --        , ("M-k", spawn "xdotool key Up")
 --        , ("M-l", spawn "xdotool key Right")
