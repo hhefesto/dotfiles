5870347c9bb00942a3df76e6a3a0070aebd5b742
hhefesto <dherrera@fpcomplete.com>
2022-10-28 13:10:41 -0500

    Clean and increased font size of xmobar

--- custom.el
+++ custom.el
@@ -4,6 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
+ '(ignored-local-variable-values
+   '((haskell-process-use-ghci . t)
+     (haskell-indent-spaces . 4)))
  '(safe-local-variable-values '((eval turn-off-auto-fill))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
